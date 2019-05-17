cdb8e58931d3c05aef7d8baa080383aea28c2a24
newtype94 <newtype94@korea.ac.kr>
2019-05-17 21:44:42 +0900

    promise 적용

--- routes/_home.js
+++ routes/_home.js
@@ -20,16 +20,43 @@ router.get('/home', function(req, res, next) {
   let now = new Date();
   now = now.toLocaleDateString();
 
-  Notice.find({
-  }).sort({
-    date: -1
-  }).limit(5).exec(function(err, notice) {
-    Board.find({
-      field: 1
-    }).sort({
-      board_date: -1
-    }).limit(10).exec(function(err, poliNew) {
-      if (err) throw err;
+  let noticeDB; //noticeFind에서 값 할당
+  let poliNewDB; //poliNewFind에서 값 할당
+  let poliHotDB; //poliHotFind에서 값 할당
+  let login; //surveyDoneFind에서 값 할당
+
+  function noticeFind() {
+    return new Promise((resolve, reject) => {
+      Notice.find({}).sort({
+        date: -1
+      }).limit(5).exec((err, notice) => {
+        if (err)
+          reject(new Error(err));
+        else {
+          noticeDB = notice;
+          resolve();
+        }
+      });
+    });
+  }
+  function poliNewFind() {
+    return new Promise((resolve, reject) => {
+      Board.find({
+        field: 1
+      }).sort({
+        board_date: -1
+      }).limit(7).exec((err, poliNew) => {
+        if (err)
+          reject(new Error(err));
+        else {
+          poliNewDB = poliNew;
+          resolve();
+        }
+      });
+    });
+  }
+  function poliHotFind() {
+    return new Promise((resolve, reject) => {
       Board.find({
         field: 1,
         like_number: {
@@ -37,69 +64,96 @@ router.get('/home', function(req, res, next) {
         }
       }).sort({
         board_date: -1
-      }).limit(10).exec(function(err, poliHot) {
-        if (err) throw err;
-        if (checkLogin(sessionUser) == 2) {
-          SurveyDone.findOne({
-            done_people: sessionUser.idK,
-            date: now
-          }, function(err, what) {
-            if (err) throw err;
-            if (what) { //SurveyDone에 있다 = 오늘 설문에 참여했다
-              res.render('home', {
-                message: req.flash('message'),
-                login: 2,
-                survey: null,
-                poliNew: poliNew,
-                poliHot: poliHot,
-                notice: notice
-              });
-            } else { //SurveyDone에 없다 = TodaySurvey에서 오늘 것 찾아준다
-              TodaySurvey.findOne({
-                date: now
-              }, function(err, surveyDB) {
-                if (err) {
-                  console.log(err);
-                  res.render("error");
-                } else if (surveyDB) { //
-                  let survey = {};
-                  survey["firstQ"] = surveyDB.firstQ;
-                  survey["_id"] = surveyDB._id;
-                  res.render('home', {
-                    message: req.flash('message'),
-                    login: 2,
-                    survey: survey,
-                    poliNew: poliNew,
-                    poliHot: poliHot,
-                    notice: notice
-                  });
-                } else {
-                  req.flash('message', '오늘 설문이 아직 안 올라왔습니다.');
-                  res.render('home', {
-                    message: req.flash('message'),
-                    login: 2,
-                    survey: null,
-                    poliNew: poliNew,
-                    poliHot: poliHot,
-                    notice: notice
-                  });
-                }
-              });
-            }
-          });
-        } else {
-          res.render('home', {
-            message: req.flash('message'),
-            login: checkLogin(sessionUser),
-            survey: null,
-            poliNew: poliNew,
-            poliHot: poliHot,
-            notice: notice
-          });
+      }).limit(7).exec((err, poliHot) => {
+        if (err)
+          reject(new Error(err));
+        else {
+          poliHotDB = poliHot;
+          resolve();
         }
       });
     });
-  });
+  }
+  function surveyDoneFind() {
+    return new Promise((resolve, reject) => {
+      if (checkLogin(sessionUser) == 2) {
+        login=2;
+        SurveyDone.findOne({
+          done_people: sessionUser.idK,
+          date: now
+        }, (err, what) => {
+          if (err)
+            reject(new Error(err));
+          else {
+            if (what) { //SurveyDone에 있다 = 오늘 설문에 참여했다
+              resolve(true);
+            } else { //SurveyDone에 없다 = TodaySurvey에서 오늘 것 찾아준다
+              resolve(false);
+            }
+          }
+        });
+      } else {
+        login = checkLogin(sessionUser);
+        resolve(true); //login==2가 아니라면 설문을 줄 필요가 없으므로
+      }
+    });
+  }
+  //survey find + res.render
+  function surveyFind(surveydone) {
+    if (surveydone) {
+      res.render('home', {
+        message: req.flash('message'),
+        login: login,
+        survey: null,
+        poliNew: poliNewDB,
+        poliHot: poliHotDB,
+        notice: noticeDB
+      });
+    } else {
+      TodaySurvey.findOne({
+        date: now
+      }, (err, surveyDB) => {
+        if (err) {
+          console.log(err);
+          res.render("error");
+        } else if (surveyDB) { //설문 있음
+          let survey = {};
+          survey["firstQ"] = surveyDB.firstQ;
+          survey["_id"] = surveyDB._id;
+          res.render('home', {
+            message: req.flash('message'),
+            login: login,
+            survey: survey,
+            poliNew: poliNewDB,
+            poliHot: poliHotDB,
+            notice: noticeDB
+          });
+        } else { //설문이 있음
+          req.flash('message', '오늘 설문이 아직 안 올라왔습니다.');
+          res.render('home', {
+            message: req.flash('message'),
+            login: login,
+            survey: null,
+            poliNew: poliNewDB,
+            poliHot: poliHotDB,
+            notice: noticeDB
+          });
+        }
+      });
+    }
+  }
+
+  noticeFind()
+    .catch(err=> {console.log(err);})
+    .then(poliNewFind)
+    .catch(err=> {console.log(err);})
+    .then(poliHotFind)
+    .catch(err=> {console.log(err);})
+    .then(surveyDoneFind)
+    .catch(err=> {console.log(err);})
+    .then(surveyFind)
+    .catch(err=> {console.log(err);});
+
 });
 
 module.exports = router;
